4b2b0fee58cb2addd81c20cb1f1a36788fedc4aa
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-09-12 15:33:04 +0300

    initial implementation

---
+++ httpserver/index.js
@@ -0,0 +1,22 @@
+var path = require('path')
+module.exports = {
+  id: 'httpserver',
+  createPort: require('ut-port-httpserver'),
+  logLevel: 'trace',
+  api: ['account'],
+  port: 8009,
+  bundle: 'account',
+  dist: path.resolve(__dirname, '../dist'),
+  routes: {
+    rpc: {
+      method: '*',
+      path: '/rpc/{method?}',
+      config: {
+        auth: false
+      }
+    }
+  },
+  start: function () {
+
+  }
+}
