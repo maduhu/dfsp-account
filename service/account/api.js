b7efd74a3759d822c56a13a4b7af570e1e6cdbf6
Dimitar Timofeev <dimitar.timofeev@softwaregroup-bg.com>
2016-11-15 15:10:11 +0200

    fix: add initial validation object for account.account.get method

--- service/account/api.js
+++ service/account/api.js
@@ -1,5 +1,13 @@
 var joi = require('joi')
 module.exports = {
+  'account.get': {
+    // tags: ['tag1', 'tag2'],
+    description: '',
+    notes: '',
+    auth: false,
+    params: joi.any(),
+    result: joi.any()
+  },
   'account.add': {
     // tags: ['tag1', 'tag2'],
     description: '',
