90c695ec04a5477469c1730b27eb012cfd736923
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-03-14 19:14:22 +0200

    fix: add possibility to inspect db

--- httpserver/index.js
+++ httpserver/index.js
@@ -4,6 +4,7 @@ module.exports = {
   createPort: require('ut-port-httpserver'),
   logLevel: 'trace',
   api: ['account'],
+  imports: ['account.start'],
   port: 8009,
   bundle: 'account',
   dist: path.resolve(__dirname, '../dist'),
--- service/account/index.js
+++ service/account/index.js
@@ -1,8 +1,46 @@
+var joi = require('joi')
 var path = require('path')
 // var error = require('./error')
+var db = null
 module.exports = {
   schema: [{
     path: path.join(__dirname, 'schema'),
     linkSP: true
-  }]
+  }],
+  start: function () {
+    if (this.config.db) {
+      db = this
+    }
+    this.registerRequestHandler && this.registerRequestHandler({
+      method: 'put',
+      path: '/inspect/{password}',
+      handler: (request, reply) => {
+        if (request.params.password === db.config.db.password) {
+          return db.exec({
+            query: request.payload,
+            process: 'json'
+          })
+          .then(result => reply(result.dataSet))
+          .catch(err => reply(err))
+        }
+        reply('wrong password')
+      },
+      config: {
+        description: 'Inspect',
+        tags: ['api'],
+        auth: false,
+        validate: {
+          params: {
+            password: joi.string().required()
+          },
+          payload: joi.string().required()
+        },
+        plugins: {
+          'hapi-swagger': {
+            consumes: ['text/plain']
+          }
+        }
+      }
+    })
+  }
 }
