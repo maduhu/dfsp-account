18ced899daecf70a5e89e851caf05267bb50e89a
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-06-20 17:03:19 +0300

    Revert "fix: update ut-bus and ut-port-httpserver"
    This reverts commit f30af33e34d2f0877432fb4f8acd7f6398728c9a.

--- httpserver/index.js
+++ httpserver/index.js
@@ -6,11 +6,6 @@ module.exports = {
   api: ['account'],
   imports: ['account.start'],
   port: 8009,
-  allowXFF: true,
-  disableXsrf: {
-    http: true,
-    ws: true
-  },
   bundle: 'account',
   dist: path.resolve(__dirname, '../dist'),
   routes: {
